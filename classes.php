0a076b947361a3787215c354ecd4735a1ab7ae26
Barrack Makedi <barrackmakedi@gmail.com>
2025-09-02 22:22:51 +0300

    create basic class

---
+++ classes.php
@@ -0,0 +1,13 @@
+<?php 
+
+//create a class
+class Sample{
+    public function greet(){
+        return "Hello from Sample class! <br>";
+    }
+    public function week_day(){
+    return "Today is " . date("l");
+}
+}
+
+?>
